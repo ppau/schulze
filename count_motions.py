eceea2e9d8d79a0e17060520249efeaba5fb025b
Brendan Molloy <brendan@bbqsrc.net>
2012-08-01 01:13:11 +1000

    Improved significantly.

--- count_motions.py
+++ count_motions.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import re
 
 from collections import Counter, defaultdict
 
@@ -15,35 +16,46 @@ for vote in votes:
 
 
 def calc_motions(obj):
-	# Sorry, I sort like a crazy man. And this barely works.
-	keys = [i.rsplit('-', 1)[0] + '-' + ( max(0, 2-len(i.rsplit('-',1)[-1])) )*'0' + i.rsplit('-', 1)[-1] for i in obj.keys()]
-	keys.sort()
+	# Sorry, I sort like a crazy man.
+	x = re.compile(r"(.*?)-(\d+)([a-z])?")
+	y = []
+	for key in obj.keys():
+		rx = x.findall(key)
+		if len(rx) == 0:
+			y.append([key, '0', -1, ''])
+		else:	
+			y.append([key, rx[0][0], int(rx[0][1]), rx[0][2]])
+	for i in reversed(range(1,4)):
+		y = sorted(y, key=lambda l: l[i])
+	keys = [i[0] for i in y]
 
 	for name in keys:
-		# If it worked, I wouldn't need to do this thing here.
-		if name.startswith('quo'):
-			name = "quorum"
-		x = obj[name.replace('0', '')]
+		x = obj[name]
 
-
-		sys.stdout.write("- %s: " % name)
+		sys.stdout.write("%s: " % name)
 		res = (x['yes'] / (x['yes'] + x['no']) * 100)
 		
 		# I screwed up the HTML form.
 		# Option 1 = "yes"
 		# Option 2 = "no"
-		if name != "motion-01":
+		if name != "motion-1":
 			if res > 50:
 				print("PASSED")
 			else:
 				print("FAILED")
+		else:
+			if res > 50:
+				print("NO CHANGE")
+			else:
+				print("CHANGE ADOPTED")
 		
-		if name == "motion-01":
-			print("\n{:>7}: {:<3} {}".format('Opt 1', x['yes'], "(%.2f%%)" % (x['yes'] / (x['yes'] + x['no']) * 100)))
+		if name == "motion-1":
+			print("{:>7}: {:<3} {}".format('Opt 1', x['yes'], "(%.2f%%)" % (x['yes'] / (x['yes'] + x['no']) * 100)))
 			print("{:>7}: {:<3} {}".format('Opt 2', x['no'], "(%.2f%%)" % (x['no'] / (x['yes'] + x['no']) * 100)))
 		else:
 			for choice in ["yes", "no"]:
 				print("{:>7}: {:<3} {}".format(choice, x[choice], "(%.2f%%)" % (x[choice] / (x['yes'] + x['no']) * 100)))
+		print()
 
 if __name__ == "__main__":
 	calc_motions(out)
